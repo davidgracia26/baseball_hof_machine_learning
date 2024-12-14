97bf6b60e700c26324e9b4722516f59323078513
David <david.gracia26@gmail.com>
2024-12-14 14:41:31 -0600

    add dataframe for all models at once

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from preprocess import get_df_for_modeling
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import xgboost as xgb
+import lightgbm as lgb
 
 df = get_df_for_modeling()
 
-print("df.corr()")
-print(df.corr())
+# print("df.corr()")
+# print(df.corr())
 
 label_name = "inducted"
 all_cols = list(df.columns.values)
@@ -28,6 +31,8 @@ print(label_col)
 
 X = df[feature_cols]
 y = df[label_col]
+
+# print(y.value_counts())
 
 # correlation_matrix = X.corr()
 
@@ -43,28 +48,44 @@ X_train = ss_train.fit_transform(X_train)
 ss_test = StandardScaler()
 X_test = ss_test.fit_transform(X_test)
 
-# clf = DecisionTreeClassifier()
-# clf = RandomForestClassifier()
-# clf = GradientBoostingClassifier()
-# clf = LogisticRegression()
-# clf = LinearSVC()
-clf = GaussianNB()
-# clf = KNeighborsClassifier()
-# clf = MLPClassifier()
-# clf = OneClassSVM(nu=0.05, kernel='rbf',average=None)
+models = {}
 
-clf = clf.fit(X_train, y_train.values.ravel())
+models["Decision Tree Classifier"] = DecisionTreeClassifier()
+models["Random Forest Classifier"] = RandomForestClassifier()
+models["Gradient Boosting Classifier"] = GradientBoostingClassifier()
+models["XGBoost"] = xgb.XGBClassifier()
+models["LightGBM"] = lgb.LGBMClassifier()
+models["Logistic Regression"] = LogisticRegression()
+models["Linear SVC"] = LinearSVC()
+models["Gaussian NB"] = GaussianNB()
+models["K Neighbors Classifier"] = KNeighborsClassifier()
+models["MLP Classifier"] = MLPClassifier()
 
-y_pred = clf.predict(X_test)
+accuracy, precision, recall = {}, {}, {}
+
+for key in models.keys():
+    models[key].fit(X_train, y_train.values.ravel())
+
+    predictions = models[key].predict(X_test)
+
+    accuracy[key] = metrics.accuracy_score(y_test, predictions)
+    precision[key] = metrics.precision_score(y_test, predictions)
+    recall[key] = metrics.recall_score(y_test, predictions)
+
+
+df_model = pd.DataFrame(
+    index=models.keys(), columns=["Accuracy", "Precision", "Recall"]
+)
+df_model["Accuracy"] = accuracy.values()
+df_model["Precision"] = precision.values()
+df_model["Recall"] = recall.values()
+
+df_sorted = df_model.sort_values(by="Precision", ascending=False)
+
+print(df_sorted)
 
 # get rid of the steroid users with a column
 # find playerID and if they played during the steroid era and won awards
-
-print(metrics.confusion_matrix(y_test, y_pred))
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-print("Precision:", metrics.precision_score(y_test, y_pred))
-print("Recall", metrics.recall_score(y_test, y_pred))
-# print("F1", metrics.f1_score(y_test, y_pred))
 
 # A system with high recall but low precision returns most of the relevant items, but the proportion of returned results that are incorrectly labeled is high.
 # A system with high precision but low recall is just the opposite, returning very few of the relevant items, but most of its predicted labels are correct when compared to the actual labels.
